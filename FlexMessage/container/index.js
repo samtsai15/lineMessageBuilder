a817b4b2e59c7f6fdff9fdf6317d5905f16a386b
Feng Tsai <feng.tsai@bigdata.com.tw>
2019-03-27 16:39:01 +0800

    release 0.0.2-alpha

--- FlexMessage/container/index.js
+++ FlexMessage/container/index.js
@@ -4,4 +4,6 @@ const Body = require('./Body');
 const Footer = require('./Footer');
 const Style = require('./Style');
 
-module.exports = { Header, Hero, Body, Footer, Style }
+module.exports = {
+  Header, Hero, Body, Footer, Style,
+};
